c7423dbf366c0114eaf7dda70ec46fdd6d539d4d
adyy_reaa <156661529+AdysonReales@users.noreply.github.com>
2025-12-09 21:58:23 +0800

    Time: 1 ms (93.11%) | Memory: 42.7 MB (8.27%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,9 @@
+import java.lang.Math;
+
+class Solution {
+    public int mySqrt(int x) {
+        double number = Math.sqrt(x);
+        int result = (int) number;
+        return result;
+    }
+}
